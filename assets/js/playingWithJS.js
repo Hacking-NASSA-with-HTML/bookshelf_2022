f3359bcd5b50320866759e6c95df0b37628b9b28
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-03 02:37:19 +0400

    feat: add pictures to books

--- assets/js/playingWithJS.js
+++ assets/js/playingWithJS.js
@@ -9,19 +9,22 @@ const grabTheMainButton = document.getElementById('addBookButtonDiv')
 
 const warehouse = [
     {
-        title: 'Book Title-123',
+        name: 'Matrix',
+        title: 'Matrix',
         content: 'Lorem ipsum dolor sit amet',
         date: '2009'
     },
 
     {
-        title: 'Book Title-456',
+        name: 'Scarface',
+        title: 'Scarface',
         content: 'the quick brown fox jumped',
         date: '2018'
     },
 
     {
-        title: 'Book Title-789',
+        name: 'Terminator',
+        title: 'Terminator',
         content: 'The jumped fox fell in a pit',
         date: '2020'
     }
@@ -33,8 +36,11 @@ const posts = books.map(book => `
     <div class="bookItem">
         <div class="bookTitleAndImage">
             <h3 class="name">${book.title}</h3>
+            <img src="./assets/img/${book.name}.jpg" alt="${book.name}" class="picture">
+        </div>
+        <div class="description">
             <p class="description-paragraph">${book.content}</p>
-            <p class="description-paragraph">${book.date}</p>
+            <p class="description-paragraph">${book.date}</p></div>
         </div>
     </div>
     `)
@@ -50,13 +56,16 @@ function addBookToShelf() {
     books.unshift(warehouse[counter])
     console.log(books)
     const posts = books.map(book => `
-        <div class="bookItem">
-            <div class="bookTitleAndImage">
-                <h3 class="name">${book.title}</h3>
-                <p class="description-paragraph">${book.content}</p>
-                <p class="description-paragraph">${book.date}</p>
-            </div>
+    <div class="bookItem">
+        <div class="bookTitleAndImage">
+            <h3 class="name">${book.title}</h3>
+            <img src="./assets/img/${book.name}.jpg" alt="${book.name}" class="picture">
         </div>
+        <div class="description">
+            <p class="description-paragraph">${book.content}</p>
+            <p class="description-paragraph">${book.date}</p></div>
+        </div>
+    </div>
         `)
     counter++
     document.querySelector('#bookKeeperDiv').innerHTML = posts.join().trim()
