4788ee42fc62b2c0b593579a4f725e477aabf244
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-02 13:27:11 +0400

    feat: add starting logic to the app

--- assets/js/playingWithJS.js
+++ assets/js/playingWithJS.js
@@ -7,14 +7,7 @@ const grabTheMainButton = document.getElementById('addBookButtonDiv')
 //     console.log('Hi !')
 // }
 
-
-const books = [
-    {
-        title: 'this-is-title-123',
-        content: 'Lorem ipsum dolor sit amet',
-        date: '12.8.2009'
-    },
-
+const warehouse = [
     {
         title: 'this-is-title-456',
         content: 'the quick brown fox jumped',
@@ -28,16 +21,44 @@ const books = [
     }
 ]
 
-const posts = books.map(book => `
-  <div>
-    <h2>${book.title}</h2>
-    <p>${book.content}</p>
-    <time>${book.date}</time>
-  </div>
-`)
+const books = [
+    {
+        title: 'this-is-title-123',
+        content: 'Lorem ipsum dolor sit amet',
+        date: '12.8.2009'
+    },
+]
 
+const posts = books.map(book => `
+  <div class="bookItem">
+            <div class="bookTitleAndImage">
+                <h3 class="name">${book.title}</h3>
+                <p class="description-paragraph">${book.content}</p>
+                <p class="description-paragraph">${book.date}</p>
+            </div>
+        </div>
+        `)
 
 grabTheMainButton.addEventListener('click', addBookToShelf)
+let counter = 0;
 function addBookToShelf() {
+    if (counter === warehouse.length) {
+        alert("The warehouse is empty!");
+        return
+    }
+    books.unshift(warehouse[counter])
+    console.log(books)
+    const posts = books.map(book => `
+        <div class="bookItem">
+            <div class="bookTitleAndImage">
+                <h3 class="name">${book.title}</h3>
+                <p class="description-paragraph">${book.content}</p>
+                <p class="description-paragraph">${book.date}</p>
+            </div>
+        </div>
+        `)
+    counter++
     document.querySelector('#bookObjectKeeper').innerHTML = posts.join();
 }
+
+document.querySelector('#bookObjectKeeper').innerHTML = posts.join();
